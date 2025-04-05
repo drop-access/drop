ebf8878973cb3dbe602dc5948615f1d19526cf51
Pawan <pawanbajaj2012@gmail.com>
2025-04-05 14:25:23 +0800

    init with frontend

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Drop | Exclusive Drops for Events & Products',
+  description: 'Join exclusive drops for events, sneakers, and more.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
